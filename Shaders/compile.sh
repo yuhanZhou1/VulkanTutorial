24d0e88dc38ffcf79094dd5b22bee02a9035d98b
yuhanZhou1 <45342198+yuhanZhou1@users.noreply.github.com>
2022-12-18 16:46:32 +0800

    Part5 - Shader Modules

---
+++ Shaders/compile.sh
@@ -0,0 +1,2 @@
+/Users/yuhan/VulkanSDK/1.3.224.1/macOS/bin/glslc shader.vert -o vert.spv
+/Users/yuhan/VulkanSDK/1.3.224.1/macOS/bin/glslc shader.frag -o frag.spv
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <optional>
 #include <set>
+#include <fstream>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -104,6 +105,8 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        //
+        createGraphicsPipeline();
     }
 
     void mainLoop() {
@@ -507,6 +510,59 @@ private:
                 throw std::runtime_error("failed to create image views!");
             }
         }
+    }
+    
+    void createGraphicsPipeline(){
+        auto vertShaderCode = readFile("/Users/yuhan/XcodeProject/VulkanTutorial/VulkanTutorial/Shaders/vert.spv");
+        auto fragShaderCode = readFile("/Users/yuhan/XcodeProject/VulkanTutorial/VulkanTutorial/Shaders/frag.spv");
+        
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+        
+        // 着色器阶段创建
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertShaderStageInfo.module = vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragShaderStageInfo.module = fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+        VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+        
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
+    }
+    
+    static std::vector<char> readFile(const std::string& filename) {
+        std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open()) {
+            throw std::runtime_error("failed to open file!");
+        }
+        size_t fileSize = (size_t) file.tellg();
+        std::vector<char> buffer(fileSize);
+        file.seekg(0);
+        file.read(buffer.data(), fileSize);
+        file.close();
+
+        return buffer;
+    }
+    
+    VkShaderModule createShaderModule(const std::vector<char>& code) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+        
+        VkShaderModule shaderModule;
+        if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create shader module!");
+        }
+        
+        return shaderModule;
     }
 };
 
